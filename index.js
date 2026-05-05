1fc0278371c085fecbcc0cbaa5b86c0975484416
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-05-05 08:38:56 -0400

    fixed formatting issue

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
       return 'Thank you so much.';
-      case 'not as generous':
-        return 'Thank you.';
-        default:
-          return 'Bye.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
   }
 }
